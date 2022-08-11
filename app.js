a0833c697004324aa1201acab13b050fbc251f4f
Illia <viziter.1994@mail.ru>
2022-08-12 00:35:23 +0300

    feat: add main page

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('hi')
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputs = form.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.classList.remove("text-field__input_valid");
+    input.classList.remove("text-field__input_invalid");
+    input.nextElementSibling.textContent = "";
+    if (input.checkValidity()) {
+      input.classList.add("text-field__input_valid");
+      input.nextElementSibling.textContent = "Отлично!";
+    } else {
+      input.classList.add("text-field__input_invalid");
+      input.nextElementSibling.textContent = input.validationMessage;
+    }
+  });
+});
